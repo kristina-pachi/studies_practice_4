52d6fe8d4a55db5f27570fd551dd06626b25b1d8
kristina-pachi <kristina.pachi.04@bk.ru>
2026-05-25 21:47:54 +0300

    Настройка conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,28 +1,30 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
+import os
+import sys
 
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+# Путь к корневой директории проекта
+sys.path.insert(0, os.path.abspath("../.."))
 
 project = 'Api OpenWeatherMap'
 copyright = '2026, Kristina Pachi'
 author = 'Kristina Pachi'
 release = '1.0'
 
-# -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-
-extensions = []
+extensions = [
+    'sphinx.ext.autodoc',  # Автоматическое извлечение docstrings
+    'sphinx.ext.napoleon',  # Поддержка Google/NumPy стилей (не мешает reST)
+    'sphinx.ext.viewcode',  # Показывать исходный код в документации
+    'sphinx.ext.todo',  # Поддержка TODO
+]
 
 templates_path = ['_templates']
-exclude_patterns = []
+
+exclude_patterns = [
+    '_build',
+    'Thumbs.db',
+    '.DS_Store',
+]
 
 language = 'ru'
 
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
